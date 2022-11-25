7ca54a9602433b3da9160272e652c101607dfaa2
Surya Teja Reddy <74102066+surya-teja-222@users.noreply.github.com>
2022-11-25 23:20:35 +0530

    fix nearestLandPrice speed

--- backend/helpers/nearestLandPrice.py
+++ backend/helpers/nearestLandPrice.py
@@ -18,13 +18,18 @@ def land_price_nearest(latitude, longitude):
         lats.append(df['lat'][i])
         lngs.append(df['lng'][i])
         price.append(df['pricePersqft'][i])
-        label.append(rgc.reverseGeoCode(df['lat'][i], df['lng'][i]))
         
 
-    lat_u, long_u , price_u, label = sorted(zip(*set(zip(lats, lngs, price, label))))
 
+    lat_u, long_u , price_u= sorted(zip(*set(zip(lats, lngs, price))))
 
-    return (list(lat_u)[:10], list(long_u)[:10], list(price_u)[:10] , list(label)[:10])
+    lat_u , long_u , price_u = list(lat_u)[:10], list(long_u)[:10], list(price_u)[:10]
+
+    label = []
+    for i in range(len(lat_u)):
+        label.append(rgc.reverseGeoCode(lat_u[i], long_u[i]))
+
+    return (lat_u , long_u , price_u, label)
 
     
 
--- backend/helpers/reverseGeoCode.py
+++ backend/helpers/reverseGeoCode.py
@@ -1,16 +1,24 @@
 import os
 import pandas as pd
-
+import requests
+import dotenv
+dotenv.load_dotenv()
 
 # URL = f'https://revgeocode.search.hereapi.com/v1/revgeocode?at={48.2181679}%2C{16.3899064}&lang=en-US&apiKey={HERE_API_KEY}'
 
 def reverseGeoCode(lat, long):
     MAP_QUEST_KEY = os.environ.get('MAP_QUEST_KEY')
-    URL = f'http://www.mapquestapi.com/geocoding/v1/reverse?key={MAP_QUEST_KEY}&location={30.333472},{-81.470448}'
+    URL = f'http://www.mapquestapi.com/geocoding/v1/reverse?key={MAP_QUEST_KEY}&location={lat},{long}'
 
     response = requests.get(URL)
     data = response.json()
-    return data['results'][0]["locations"][0]["adminArea5"]
+    str1 = data['results'][0]["locations"][0]["adminArea6"] if data['results'][0]["locations"][0]["adminArea6"] else ' '
+    str2 = data['results'][0]["locations"][0]["adminArea5"] if data['results'][0]["locations"][0]["adminArea5"] else ' '
+    return str1 + ' ' + str2
 
 
 
+# df = pd.read_csv('../static/LandIndia.csv')
+
+# for i in range(20):
+#     print(reverseGeoCode(df['lat'][i], df['lng'][i]))
